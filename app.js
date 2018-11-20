1e98da7651aae799381390975b72b204e60cc107
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-20 16:28:09 -0600

    De nuevo

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose"),
-    port            = 4000;
+    mongoose        = require("mongoose");
 
 //mongoose.connect("mongodb://jorge:jorge007@ds155862.mlab.com:55862/gourou", {useNewUrlParser: true});
-//mongoose.connect("mongodb://localhost/Descubramos", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));   
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.render("landing/landing");
+    res.render("landing");
 });
+
 
 app.listen(process.env.PORT,process.env.IP, function(){
     console.log("Esta vivo!!!!")
-});
+})
 //process.env.PORT,process.env.IP
