7b47cfd3f86e395a1f976510545080f5ebc931c4
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-20 02:17:04 -0600

    cuarto intento de deploy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));   
 
 app.get("/", function(req, res){
-    res.render("landing")
+    res.render("landing.ejs")
 })
 
 app.listen(process.env.PORT,process.env.IP, function(){
