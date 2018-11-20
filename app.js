15ef8514352fc51ed879cf5647696f1a6bf45e71
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-20 01:45:23 -0600

    Primera parte de la landing

---
+++ app.js
@@ -0,0 +1,22 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    port            = 4000
+
+//mongoose.connect("mongodb://jorge:jorge007@ds155862.mlab.com:55862/gourou", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/Descubramos", {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));   
+
+app.get("/", function(req, res){
+    res.render("landing")
+})
+
+
+
+app.listen(port, function(){
+    console.log("Esta vivo!!!!")
+})
+//process.env.PORT,process.env.IP
